5e599be3e2b03eb3c4226a7331558ab913cf63e8
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2024-12-19 14:53:44 +0500

    Single product

--- src/components/category/get-started.jsx
+++ src/components/category/get-started.jsx
@@ -1,26 +1,33 @@
-import Link from 'next/link'
 import React from 'react'
 import BlockContent from "@sanity/block-content-to-react";
+import Image from 'next/image';
 
 function Get_Started({ data }) {
     return (
         <section className='py-16'>
+
             <div className='container mx-auto px-4'>
-                <div>
-                    <h2 className='md:text-4xl text-3xl font-semibold text-title_Clr text-center'>
-                        {data?.secound_title}
-                    </h2>
-                    <BlockContent blocks={data?.content} className='text-lg font-normal text-txt_Clr max-w-[890px] mx-auto mt-5 flex flex-col gap-3'/>
-                </div>
-                <div className='mt-10 flex justify-center gap-3'>
-                    <Link href="#" className='bg-primary hover:bg-secondary md:px-8 py-4 text-sm font-semibold text-white block w-[193px] text-center border border-primary hover:border-secondary rounded-[5px]'>
-                        Request for Quote
-                    </Link>
-                    <Link href="#" className='bg-white hover:bg-primary md:px-8 py-4 text-sm font-semibold text-primary hover:text-white block w-[193px] text-center border border-primary rounded-[5px]'>
-                        Call US Now
-                    </Link>
-                </div>
+                <h2 className='md:text-[51px] md:leading-normal text-3xl font-bold text-title_Clr text-center mb-4'>
+                    Custom Retail Boxes Gallery
+                </h2>
             </div>
+            <div className='flex flex-row items-center justify-center  -space-x-8'>
+                <div className='w-fit h-fit rounded-[32px] z-0'>
+                    <Image src="/images/product/boxgal1.png" alt='boxgal' width={147} height={398} className='object-cover object-center rounded-[32px] ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-10'>
+                    <Image src="/images/product/boxgal2.png" alt='boxgal' width={339} height={457} className='object-cover object-center rounded-[32px] ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-20'>
+                    <Image src="/images/product/boxgal3.png" alt='boxgal' width={398} height={536} className='object-cover object-center rounded-[32px] ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-30'>
+                    <Image src="/images/product/boxgal4.png" alt='boxgal' width={481} height={647} className='object-cover object-center rounded-[32px] ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-20'>
+                    <Image src="/images/product/boxgal5.png" alt='boxgal' width={398} height={536} className='object-cover object-center rounded-[32px]  ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-10'>
+                    <Image src="/images/product/boxgal6.png" alt='boxgal' width={339} height={457} className='object-cover object-center rounded-[32px]  ' /></div>
+                <div className='w-fit h-fit rounded-[32px] z-0'>
+                    <Image src="/images/product/boxgal7.png" alt='boxgal' width={147} height={398} className='object-cover object-center rounded-[32px]  ' /></div>
+            </div>
+
         </section>
     )
 }
