d16010a5f9d1d7b875712a6bf582944f19956863
syed-zunair-gillani <dev.zunaair@gmail.com>
2025-01-14 19:34:26 +0500

    udate

--- src/components/home/mainSlider.jsx
+++ src/components/home/mainSlider.jsx
@@ -16,22 +16,38 @@ export default function MainSlider() {
     <section className="Main_slider">
       <Slider {...settings}>
         {[0, 1, 2].map((_item, idx) => {
-          return <div key={idx}>
-            <div className='md:py-44 py-24 relative bg-center bg-no-repeat bg-cover before:content-[""] before:absolute before:w-full before:bg-gradient-to-r before:bg-transparent  before:from-black/90 before:from-30% before:via-black/70  before:h-full before:top-0' style={{ backgroundImage: `url("/images/banner.png")` }}>
-              <div className='container mx-auto px-4 relative z-10'>
-                <div className='md:w-1/2 w-full'>
-                  <h1 className='md:text-6xl md:leading-tight text-3xl font-bold text-white mb-10'>
-                    Custom cardboard packaging solutions
-                  </h1>
-                  <Link href="#" className='bg-primary hover:bg-white px-5 py-4 text-lg font-medium text-white hover:text-primary rounded-full border-2 border-primary inline-flex items-center gap-2'>
-                    Enquire Now <FaChevronRight />
-                  </Link>
+          return (
+            idx === 0 ? <div class="video-container" key={idx}>
+              <iframe src="https://www.youtube.com/embed/Yj2iELI6OeI?autoplay=1&mute=1&loop=1&playlist=Yj2iELI6OeI&playsinline=1&controls=0&modestbranding=1" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+              <div className='md:py-44 py-24 relative bg-center bg-no-repeat bg-cover before:content-[""] before:absolute before:w-full before:bg-gradient-to-r before:bg-transparent  before:from-black/90 before:from-30% before:via-black/70  before:h-full before:top-0'>
+                <div className='container mx-auto px-4 relative z-10'>
+                  <div className='md:w-1/2 w-full'>
+                    <h1 className='md:text-6xl md:leading-tight text-3xl font-bold text-white mb-10'>
+                      Custom cardboard packaging solutions
+                    </h1>
+                    <Link href="#" className='bg-primary hover:bg-white px-5 py-4 text-lg font-medium text-white hover:text-primary rounded-full border-2 border-primary inline-flex items-center gap-2'>
+                      Enquire Now <FaChevronRight />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div> : <div key={idx}>
+              <div className='md:py-44 py-24 relative bg-center bg-no-repeat bg-cover before:content-[""] before:absolute before:w-full before:bg-gradient-to-r before:bg-transparent  before:from-black/90 before:from-30% before:via-black/70  before:h-full before:top-0' style={{ backgroundImage: `url("/images/banner.png")` }}>
+                <div className='container mx-auto px-4 relative z-10'>
+                  <div className='md:w-1/2 w-full'>
+                    <h1 className='md:text-6xl md:leading-tight text-3xl font-bold text-white mb-10'>
+                      Custom cardboard packaging solutions
+                    </h1>
+                    <Link href="#" className='bg-primary hover:bg-white px-5 py-4 text-lg font-medium text-white hover:text-primary rounded-full border-2 border-primary inline-flex items-center gap-2'>
+                      Enquire Now <FaChevronRight />
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          )
         })}
-      </Slider>
-    </section>
+    </Slider>
+    </section >
   );
 }
--- src/components/slider/center-slider.tsx
+++ src/components/slider/center-slider.tsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import React from "react";
 import Slider from "react-slick";
 import { urlForImage } from "../../../sanity/lib/image";
+import Link from "next/link";
 
 const CenterSlider = ({data}:any) => {
-  return (
+ return (
     <section className="mt-10 overflow-x-hidden">
       <Slider {...settings}>
         {data?.map((item:any, idx:number) => (
           <div key={idx} className="">
-            <figure>
+            <Link href={`/products/${item?.slug.current}`}>
               <Image
                 src={item?.image?.asset?.url}
                 alt=""
@@ -17,8 +18,8 @@ const CenterSlider = ({data}:any) => {
                 height={369}
                 className="w-full"
               />
-            </figure>
-            <h4 className="text-xl text-center mt-6">{item?.title}</h4>
+            </Link>
+            <h4 className="text-xl text-center mt-6"><Link href={`/products/${item?.slug.current}`}>{item?.title}</Link></h4>
           </div>
         ))}
       </Slider>
